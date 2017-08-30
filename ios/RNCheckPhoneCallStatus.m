9b940bf0695894271a54b089866985d83ab5ae65
Deploy <deploy@deploy.com>
2017-08-30 14:42:40 -0500

    Change to actual values

--- ios/RNCheckPhoneCallStatus.m
+++ ios/RNCheckPhoneCallStatus.m
@@ -8,40 +8,22 @@
 @implementation RNCheckPhoneCallStatus
 
 RCT_EXPORT_MODULE()
-// - (dispatch_queue_t)methodQueue
-// {
-//     return dispatch_get_main_queue();
-// }
+
 RCT_EXPORT_METHOD(get)
 {
-  NSString *phoneStatus = @"not on a call yo";
-  CTCallCenter *ctCallCenter = [[CTCallCenter alloc] init];
-  if (ctCallCenter.currentCalls != nil)
-  {
-    NSArray* currentCalls = [ctCallCenter.currentCalls allObjects];
-    for (CTCall *call in currentCalls)
+    CTCallCenter *ctCallCenter = [[CTCallCenter alloc] init];
+    if (ctCallCenter.currentCalls != nil)
     {
-      if(call.callState == CTCallStateConnected)
-      {
-        phoneStatus = @"Im on da phoneeeee";
-        RCTLogInfo(@"ON A CALL %@\n", phoneStatus);
-        return TRUE
-      }
+        NSArray* currentCalls = [ctCallCenter.currentCalls allObjects];
+        for (CTCall *call in currentCalls)
+        {
+            if(call.callState == CTCallStateConnected)
+            {
+                return @"PHONE_ON";
+            }
+        }
     }
-  }
-  RCTLogInfo(@"ON A CALL %@\n", phoneStatus);
-  return FALSE
+    return @"PHONE_OFF";
 }
-
-// RCT_EXPORT_METHOD(isOnCall)
-// {
-//   CTCallCenter *callCenter = [[[CTCallCenter alloc] init] autorelease];
-//   for (CTCall *call in callCenter.currentCalls)  {
-//       if (call.callState == CTCallStateConnected) {
-//           RCTLogInfo(@"++++++++===============IS ON A CALL");
-//       }
-//   }
-//   RCTLogInfo(@"++++++++===============NOT ON A CALL");
-// }
 
 @end
